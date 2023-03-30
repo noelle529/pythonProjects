dc4201391b8f053741c08110b9aaeb7d26b2895f
noelle529.github.io <noellecharter@gmail.com>
2023-03-30 18:01:14 -0400

    testing input code

--- Variables/growthDecayApp.py
+++ Variables/growthDecayApp.py
@@ -1,7 +1,11 @@
 a = input('what is the starting amount?') #the inital amount
-r = 1.0 + 0.1 #rate calculation(growth) : to make DECAY change "addtion" symbol to "subtraction"
+r = 1.0  #rate calculation(growth) : to make DECAY change "addtion" symbol to "subtraction" 
+
+r2 = input('what is the rate of growth or decay?')#rate of growth or decay can be changed
 t = input('how long in years?') # time
-x = float(a) * r # total of intial amount + rate
+x = float(a) * r + float(r2) # total of intial amount + rate
 
 total = pow(x,float(t)) #this variable returns the base raise to the power of exponent.
 print(total)
+
+
