ffa4e85d4a8bbe6a0755ed24ae9ea52b4ca85e53
noelle529.github.io <noellecharter@gmail.com>
2023-04-06 19:30:11 -0400

    slope app

---
+++ Variables/slopeApp.py
@@ -0,0 +1,7 @@
+xOne = input('What is the X one?')
+xTwo = input('What is the X two?')
+yOne = input('What is the Y one?')
+yTwo = input('What is the Y two?')
+
+total = (float(yTwo)-float(yOne))/(float(xTwo)- float(xOne))
+print(total)
