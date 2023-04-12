73eaa5cd5f3266394e6f6dbd8c0c28537f0c74f4
noelle529.github.io <noellecharter@gmail.com>
2023-04-12 12:23:49 -0400

    testing

---
+++ controlFlow/rockPaperScissorsGame.py
@@ -0,0 +1,10 @@
+# Rock, Paper, Scissors
+userInput = input('What are you going to throw?')
+
+computerInput = ['rock', 'paper', 'scissor']
+
+if userInput != 'rock' or 'paper' or 'scissor':
+    print('Please enter a valid entire')
+else:
+    
+    print(userInput)
