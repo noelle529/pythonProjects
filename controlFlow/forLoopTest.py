aa4f90efaa5b8e50c20aa80d835d86ae0f90272d
noelle529.github.io <noellecharter@gmail.com>
2023-04-20 08:47:20 -0400

    tester code

--- controlFlow/forLoopTest.py
+++ controlFlow/forLoopTest.py
@@ -1,4 +1,34 @@
+import random
 #for loop test code
-print('My name is ')
-for i in range(5):
+""" print('My name is ')
+for i in range(4,10): 
+        #skips zero and starts 4 and ends at 9
     print('Noelle five time ' + str(i))
+
+print('My name is ')
+for i in range(5): #counts up to 4 because its starts at 0
+    print('Noelle five time ' + str(i))
+
+total = 0
+for num in range(101):
+    total = total + num
+    print(total) 
+
+Step arguement range()
+first - starts the loop
+second - ends the loop
+third - how will the range step or count
+*can be used to count down or up as needed
+
+   print('My name is ')
+for i in range(2,111,2): 
+    print('Noelle five time ' + str(i)) 
+
+print('My name is ')
+for i in range(2,-111,-2): 
+    print('Noelle five time ' + str(i)) 
+    
+    """
+
+
+print(random.randint(2,111))
